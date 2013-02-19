44937f8277a3927eb3bade39fc9de1be771cc041
Nullivex <contact@nullivex.com>
2013-02-19 14:35:39 -0800

    most of #72 now everything needs fixed

---
+++ conf/log.php
@@ -0,0 +1,7 @@
+<?php
+
+$config['log']['level'] = LOG_INFO;
+$config['log']['file'] = dirname(__DIR__).'/log';
+$config['log']['format'] = '[%s] %s - %s';
+$config['log']['date_format'] = 'm/d/Y g:i:sA';
+
