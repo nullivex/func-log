3931bd17e27edc51ec6d95c57adcb1e36364469b
Nullivex <contact@nullivex.com>
2013-03-13 13:29:40 -0700

    added tests for all the functions

--- conf/log.php
+++ conf/log.php
@@ -2,6 +2,6 @@
 
 $config['log']['level'] = LOG_INFO;
 $config['log']['file'] = dirname(__DIR__).'/log';
-$config['log']['format'] = '[%s] %s - %s';
+$config['log']['format'] = '[%s] %s - %s'; //%s - date %s - level %s - message
 $config['log']['date_format'] = 'm/d/Y g:i:sA';
 
