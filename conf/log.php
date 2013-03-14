63780dfef6c5a0aafb1a0e764d01b145fe9d68ba
Nullivex <contact@nullivex.com>
2013-03-13 22:51:41 -0700

    various fixes to get client sdk connected

--- conf/log.php
+++ conf/log.php
@@ -1,7 +1,7 @@
 <?php
 
 $config['log']['level'] = LOG_INFO;
-$config['log']['file'] = dirname(__DIR__).'/log';
+$config['log']['file'] = false;
 $config['log']['format'] = '[%s] %s - %s'; //%s - date %s - level %s - message
 $config['log']['date_format'] = 'm/d/Y g:i:sA';
 
