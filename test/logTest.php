13fff986dc78ee9ca160bafb84a49676ee8e8c4b
Nullivex <contact@nullivex.com>
2013-03-13 22:51:41 -0700

    various fixes to get client sdk connected

--- test/logTest.php
+++ test/logTest.php
@@ -4,8 +4,10 @@ require_once(dirname(__DIR__).'/test_common.php');
 class FuncLogTest extends PHPUNIT_Framework_TestCase {
 
 	public function testDolog(){
+		global $_log_fh;
 		$tmpfile = tempnam('/tmp','LTS');
 		if(file_exists($tmpfile)) unlink($tmpfile);
+		$config = clone Config::$inst;
 		Config::set('log','file',$tmpfile);
 		Config::set('log','level',LOG_INFO);
 		Config::set('log','date_format','mdY');
@@ -15,6 +17,9 @@ class FuncLogTest extends PHPUNIT_Framework_TestCase {
 		$log = file_get_contents($tmpfile);
 		$this->assertEquals(18,strpos($log,'test info'));
 		unlink($tmpfile);
+		//restore original config
+		Config::$inst = $config;
+		$_log_fh = false;
 	}
 
 }
