bb95bf9007b2f28e21ca016311dfc25d9708c5d9
Colin Li <cboisworld@gmail.com>
2013-03-23 05:59:39 -0400

    added licensing

--- test/logTest.php
+++ test/logTest.php
@@ -1,10 +1,31 @@
 <?php
+/**
+ *  OpenLSS - Lighter Smarter Simpler
+ *
+ *	This file is part of OpenLSS.
+ *
+ *	OpenLSS is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Lesser General Public License as
+ *	published by the Free Software Foundation, either version 3 of
+ *	the License, or (at your option) any later version.
+ *
+ *	OpenLSS is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the 
+ *	GNU Lesser General Public License along with OpenLSS.
+ *	If not, see <http://www.gnu.org/licenses/>.
+*/
+namespace LSS;
+
 require_once(dirname(__DIR__).'/vendor/autoload.php');
 require('boot.php');
 ld('config','func/log');
 define('LOG_QUIET',true);
 
-class FuncLogTest extends PHPUNIT_Framework_TestCase {
+class FuncLogTest extends \PHPUNIT_Framework_TestCase {
 
 	public function testDolog(){
 		global $_log_fh;
