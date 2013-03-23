d2e08a6e3ce55d238e2f6be62c54010750f58a5f
nullivex <contact@nullivex.com>
2013-03-22 22:35:13 -0700

    added unit testing and path updates

--- test/logTest.php
+++ test/logTest.php
@@ -1,6 +1,8 @@
 <?php
-require_once(dirname(__DIR__).'/test_common.php');
-ld('/func/log');
+require_once(dirname(__DIR__).'/vendor/autoload.php');
+require('boot.php');
+ld('config','func/log');
+define('LOG_QUIET',true);
 
 class FuncLogTest extends PHPUNIT_Framework_TestCase {
 
@@ -8,7 +10,8 @@ class FuncLogTest extends PHPUNIT_Framework_TestCase {
 		global $_log_fh;
 		$tmpfile = tempnam('/tmp','LTS');
 		if(file_exists($tmpfile)) unlink($tmpfile);
-		$config = clone Config::$inst;
+		$config = null;
+		if(is_object(Config::$inst)) $config = clone Config::$inst;
 		Config::set('log','file',$tmpfile);
 		Config::set('log','level',LOG_INFO);
 		Config::set('log','date_format','mdY');
@@ -19,7 +22,7 @@ class FuncLogTest extends PHPUNIT_Framework_TestCase {
 		$this->assertEquals(18,strpos($log,'test info'));
 		unlink($tmpfile);
 		//restore original config
-		Config::$inst = $config;
+		if(is_object($config)) Config::$inst = $config;
 		$_log_fh = false;
 	}
 
